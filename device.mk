7e208fef389dcc2a3f061c1da34074f07e65ad34
Sean Hoyt <seanhoyt963@gmail.com>
2014-04-20 15:54:31 -0500

    Liquify

--- device.mk
+++ device.mk
@@ -190,6 +190,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     charger \
     charger_res_images
+	
+# OTA ID
+PRODUCT_PROPERTY_OVERRIDES += \
+    otaupdater.otaid=liquidmanta
 
 $(call inherit-product-if-exists, hardware/samsung_slsi/exynos5/exynos5.mk)
 $(call inherit-product-if-exists, vendor/samsung_slsi/exynos5/exynos5-vendor.mk)
